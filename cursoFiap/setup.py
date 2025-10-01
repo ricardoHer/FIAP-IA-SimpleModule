84889e04d9faad5091b62a540e13242505dae31d
Ricardo FRanco Hernandez <ricardo.hern@icloud.com>
2025-10-01 19:50:50 -0300

    deployment

--- cursoFiap/setup.py
+++ cursoFiap/setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='cursoFiap-package',
+    name='fiap-IA-SimpleModule',
     version='1.0.0',
     packages=find_packages(),
     description='Lib pdo Curso FIAP de IA para DEV\'s',
